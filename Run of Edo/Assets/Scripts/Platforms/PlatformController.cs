b5e6ac39cae46a67850d463a673797a068b38d01
Ronan <30617930+MWRonanD@users.noreply.github.com>
2019-06-24 23:41:45 +0200

    Platform spawnner

---
+++ Run of Edo/Assets/Scripts/Platforms/PlatformController.cs
@@ -0,0 +1,41 @@
+﻿using Platform;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    [SerializeField]
+    protected PlatformTypes[] platformTypes;
+    private float viewZone;
+
+    public bool IsOutView
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewZone = (Camera.main.orthographicSize * Screen.width) / Screen.height;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOutView)
+        {
+
+        }
+    }
+
+    private void GenerateNewPlatform()
+    {
+
+    }
+}
