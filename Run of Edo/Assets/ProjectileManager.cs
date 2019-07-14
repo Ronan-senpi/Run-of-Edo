d4cbb8e2b2b7f79335d1a16cc37dc782008933c8
Ronan <30617930+MWRonanD@users.noreply.github.com>
2019-07-14 23:22:49 +0200

    Range attack in progress

---
+++ Run of Edo/Assets/ProjectileManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager : Base, IManager
+{
+    PlayerController playerController;
+    protected override void Awake()
+    {
+        base.Awake();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+}
---
+++ Run of Edo/Assets/RangeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeController : MonoBehaviour
+{
+    [SerializeField]
+    float minScal = .2f;
+    [SerializeField]
+    float reduceScaleValue= .2f;
+    [SerializeField]
+    float cooldown= 1f;
+    Transform player;
+    Vector3 originalScale;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        originalScale = transform.localScale;
+    }
+    private void Update()
+    {
+        transform.position = player.position;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (transform.localScale.x > .2f)
+            {
+                transform.localScale -= new Vector3(reduceScaleValue, reduceScaleValue);
+            }
+        }
+        if (transform.localScale.x < originalScale.x)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale, cooldown * Time.deltaTime);
+        }
+    }
+
+
+}
