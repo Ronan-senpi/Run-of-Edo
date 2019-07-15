6a80f103ccc3ede7d6b877d5eff1d48b4e45fe3b
Ronan <30617930+MWRonanD@users.noreply.github.com>
2019-07-15 19:52:56 +0200

    Unstopable + stopable troublemaler

--- Run of Edo/Assets/Scripts/TroubleMakers/Shots/ShotBody.cs
+++ Run of Edo/Assets/Scripts/TroubleMakers/Shots/ShotBody.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ShotBody : MonoBehaviour, Destroyable
 {
-    public void Destroy()
+    public virtual void Destroy()
     {
         Destroy(transform.parent.gameObject);
     }
 
     // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
